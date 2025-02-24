403ed187f525cd7f7bd32c4f01245952b3cc9027
sujin <jien911@gmail.com>
2025-02-24 17:58:27 +0900

    .

--- src/main/java/com/skillbank/main/controller/HC.java
+++ src/main/java/com/skillbank/main/controller/HC.java
@@ -1,11 +1,13 @@
 package com.skillbank.main.controller;
 
+import com.skillbank.main.service.SampleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HC {
-    public static void main(String[] args) {
-
 
         @Autowired
         private SampleService sampleService;
