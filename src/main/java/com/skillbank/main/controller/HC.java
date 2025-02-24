14e1461ac1b1efd34ed093c5f0d32eebea0c7564
sujin <jien911@gmail.com>
2025-02-24 17:52:35 +0900

    Revert "Merge branch 'dev' of https://github.com/doyoon6388/skillbank into sj"
    This reverts commit 622ef0faec6cd2cc4c1c268c244711365ec0ad65, reversing
    changes made to e3f3c79f6d987da641ae94aa9d6e889d3ee5064c.

--- src/main/java/com/skillbank/main/controller/HC.java
+++ src/main/java/com/skillbank/main/controller/HC.java
@@ -1,21 +1,10 @@
 package com.skillbank.main.controller;
 
-import com.skillbank.main.service.SampleService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
 
 @Controller
 public class HC {
-    @Autowired
-    private SampleService sampleService;
+    public static void main(String[] args) {
 
-    @GetMapping("/")
-    public String index(Model model) {
-        model.addAttribute("page", "main/main.jsp");
-        return "index";
     }
-
 }
