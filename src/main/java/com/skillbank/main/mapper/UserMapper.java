e76c2c1b337cbc09e822bfbb9e742108ec55bb6b
huengs99 <lastpang0123@gmail.com>
2025-02-24 21:11:11 +0900

    [ADD] login system

---
+++ src/main/java/com/skillbank/main/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package com.skillbank.main.mapper;
+
+import com.skillbank.main.vo.UserTestVO;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface UserMapper {
+        @Select("select * from user_test_db where username = #{username} and password = #{password}")
+        UserTestVO loginValid(String username, String password);
+}
