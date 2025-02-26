499c8c7cd855ce6358d39e60e91e8cf4d9a58fd7
doyoon <doyoonkim21cm@gmail.com>
2025-02-26 13:02:43 +0900

    .

--- src/main/java/com/skillbank/main/service/MainService.java
+++ src/main/java/com/skillbank/main/service/MainService.java
@@ -5,6 +5,7 @@ import com.skillbank.main.vo.UserTestVO;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 
 @Service
 public class MainService {
